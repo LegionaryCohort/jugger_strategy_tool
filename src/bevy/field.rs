d8b367c6203b8c86aacc7f3be3572869d8a2c956
Mario Treiber <mariotreiber@gmail.com>
2025-05-24 11:05:35 +0200

    Finished background field

--- src/bevy/field.rs
+++ src/bevy/field.rs
@@ -1,21 +1,19 @@
 use bevy::{app::Plugin, color::palettes::css::*, ecs::system::Commands, prelude::*};
 use bevy_prototype_lyon::prelude::*;
-use lyon_algorithms::{
-    geom::LineSegment,
-    hatching::{HatchSegment, Hatcher, HatchingOptions, RegularHatchingPattern},
-    path::Path as LyonPath,
-};
 
 pub struct FieldPlugin;
 impl Plugin for FieldPlugin {
     fn build(&self, app: &mut bevy::app::App) {
         app.add_plugins(ShapePlugin)
-            .add_systems(Startup, setup_background);
+            .add_systems(Startup, setup_field);
     }
 }
 
-fn setup_background(mut commands: Commands) {
-    let field_border = shapes::Polygon {
+const SIZE_SCALE: f32 = 20.; // pixels per meter
+const SCALING_VECTOR: Vec3 = Vec3::splat(SIZE_SCALE);
+
+fn setup_field(mut commands: Commands) {
+    let field_corners = shapes::Polygon {
         points: [
             Vec2::new(-15., -10.),
             Vec2::new(-20., -5.),
@@ -26,73 +24,57 @@ fn setup_background(mut commands: Commands) {
             Vec2::new(20., -5.),
             Vec2::new(15., -10.),
         ]
-        .map(|p| p * 20.)
         .into_iter()
         .collect(),
         closed: true,
     };
+    let field_border = GeometryBuilder::build_as(&field_corners);
+
+    let mut center_line_builder = PathBuilder::new();
+    for marker in (-10..=-2).step_by(2) {
+        center_line_builder.move_to(Vec2::new(0., marker as f32));
+        center_line_builder.line_to(Vec2::new(0., (marker + 1) as f32));
+    }
+    for marker in (1..=9).step_by(2) {
+        center_line_builder.move_to(Vec2::new(0., marker as f32));
+        center_line_builder.line_to(Vec2::new(0., (marker + 1) as f32));
+    }
+    let center_line = center_line_builder.build();
+
+    let center_point = GeometryBuilder::build_as(&shapes::Circle {
+        radius: 0.25,
+        center: Vec2::ZERO,
+    });
+
+    let left_base = GeometryBuilder::build_as(&shapes::Circle {
+        radius: 0.5,
+        center: Vec2::new(-18., 0.),
+    });
+    let right_base = GeometryBuilder::build_as(&shapes::Circle {
+        radius: 0.5,
+        center: Vec2::new(18., 0.),
+    });
+
+    let field_shape = GeometryBuilder::new()
+        .add(&field_border)
+        .add(&center_line)
+        .add(&center_point)
+        .add(&left_base)
+        .add(&right_base)
+        .build();
+
     commands.spawn((
         ShapeBundle {
-            path: GeometryBuilder::build_as(&field_border),
+            path: field_shape,
+            transform: Transform {
+                scale: SCALING_VECTOR,
+                ..default()
+            },
             ..default()
         },
-        Stroke::new(BLACK, 5.0),
+        Stroke::new(BLACK, 0.2),
         Fill::color(LIGHT_GREEN),
     ));
-
-    // center line
-    // TODO something about this causes a panic at runtime
-    let center_line = shapes::Line(Vec2::new(0., -10.), Vec2::new(0., 10.));
-    let center_line_path = GeometryBuilder::build_as(&center_line).0;
-
-    let mut hatches = LyonPath::builder();
-    let mut hatcher = Hatcher::new();
-    hatcher.hatch_path(
-        center_line_path.iter(),
-        &HatchingOptions::DEFAULT,
-        &mut RegularHatchingPattern {
-            interval: 1.0,
-            callback: &mut |segment: &HatchSegment| {
-                hatches.add_line_segment(&LineSegment {
-                    from: segment.a.position,
-                    to: segment.b.position,
-                });
-            },
-        },
-    );
-    let hatched_path = hatches.build();
-
-    commands.spawn((
-        ShapeBundle {
-            path: Path(hatched_path),
-            ..default()
-        },
-        Stroke::new(BLACK, 3.0),
-    ));
-
-    // let mut path_builder = PathBuilder::new();
-    // path_builder.move_to(Vec2::new(0., 0.));
-    // path_builder.line_to(Vec2::new(0., 0.));
-    // path_builder.cubic_bezier_to(
-    //     Vec2::new(70., 70.),
-    //     Vec2::new(175., -35.),
-    //     Vec2::new(0., -140.),
-    // );
-    // path_builder.cubic_bezier_to(
-    //     Vec2::new(-175., -35.),
-    //     Vec2::new(-70., 70.),
-    //     Vec2::new(0., 0.),
-    // );
-    // path_builder.close();
-    // let path = path_builder.build();
-
-    // commands.spawn((
-    //     ShapeBundle {
-    //         path,
-    //         transform: Transform::from_xyz(0., 75., 0.),
-    //         ..default()
-    //     },
-    //     Stroke::new(BLACK, 10.0),
-    //     Fill::color(RED),
-    // ));
 }
+
+// TODO add zoom capabilities
