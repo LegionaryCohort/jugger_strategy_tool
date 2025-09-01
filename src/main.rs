15bcf6ead0cb03cb4333a3c0da0adf175ad2e966
Mario Treiber <mariotreiber@gmail.com>
2025-09-01 16:13:06 +0200

    Cleaned up main a bit

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
 mod bevy;
 mod leptos_app;
 
-use crate::leptos_app::App;
-use bevy::init_bevy;
-use leptos::prelude::mount_to_body;
-
 pub const RENDER_WIDTH: f32 = 990.0;
 pub const RENDER_HEIGHT: f32 = 484.0;
 
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
+    use bevy::init_bevy;
     init_bevy().run();
 }
 
 #[cfg(target_arch = "wasm32")]
 fn main() {
+    use crate::leptos_app::App;
+    use leptos::prelude::mount_to_body;
     console_error_panic_hook::set_once();
     mount_to_body(App);
 }
