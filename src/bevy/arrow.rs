36046e6136cdea0c875d41d0b3fa40a39353081c
Mario Treiber <mariotreiber@gmail.com>
2025-09-14 00:35:34 +0200

    Reorganized & extended arrow code

--- src/bevy/arrow.rs
+++ src/bevy/arrow.rs
@@ -6,15 +6,8 @@ use core::f32;
 pub struct ArrowPlugin;
 impl Plugin for ArrowPlugin {
     fn build(&self, app: &mut bevy::app::App) {
-        app
-		// .init_resource::<UnitRegistry>()
-            .add_systems(Startup, sys_spawn_test_arrows)
-            // .add_systems(
-            //     Update,
-            //     sys_sync_selection_state.run_if(resource_changed::<UnitRegistry>),
-            // )
-            // .add_systems(Update, sys_update_unit_visuals)
-			;
+        app.add_systems(Startup, sys_spawn_test_arrows)
+            .add_systems(Update, sys_update_arrow_visuals);
     }
 }
 
@@ -37,25 +30,101 @@ fn sys_spawn_test_arrows(mut commands: Commands) {
     );
 }
 
-pub fn spawn_arrow(arrow: Arrow, commands: &mut Commands) {
-    // TODO this needs a rewrite:
-    // the arrow should always start at a local 0,0 to ensure that the starting point of the arrow is
-    // also the location of the arrow
-    // this requires everything to be recentered on 'from' before making calculations
-    // also the arrow geometry should be built in the same scale as the units
-    // to ensure that scaling, zoom, etc. have a consistent effect
-    // -> check what this is for units, are they based on the meter distances from the center?
+fn sys_update_arrow_visuals(
+    mut q_arrows: Query<(&Arrow, &mut Path, &mut Transform), Changed<Arrow>>,
+) {
+    for (arrow, mut path, mut transform) in q_arrows.iter_mut() {
+        *path = calc_arrow_path(arrow);
+        *transform = arrow.get_transform();
+    }
+}
 
-    let arrow_head_result = match arrow {
-        Arrow::Straight { from, to } => calculate_arrowhead(from, to),
+pub fn spawn_arrow(arrow: Arrow, commands: &mut Commands) {
+    commands.spawn((arrow, ShapeBundle::default(), Stroke::new(BLACK, 10.)));
+}
+
+#[derive(Component, Clone, Copy, Debug)]
+pub enum Arrow {
+    // TODO from and to should take a Vec2 or a Unit
+    // add new type LocalizedArrow to handle this change for calculations
+    Straight {
+        from: Vec2,
+        to: Vec2,
+    },
+    Bezier {
+        from: Vec2,
+        to: Vec2,
+        control_from: Vec2,
+        control_to: Vec2,
+    },
+}
+impl Arrow {
+    fn localized(&self) -> Arrow {
+        match self {
+            Arrow::Straight { from, to } => Arrow::Straight {
+                from: Vec2::ZERO,
+                to: to - from,
+            },
+            Arrow::Bezier {
+                from,
+                to,
+                control_from,
+                control_to,
+            } => Arrow::Bezier {
+                from: Vec2::ZERO,
+                to: to - from,
+                control_from: control_from - from,
+                control_to: control_to - from,
+            },
+        }
+    }
+
+    fn get_transform(&self) -> Transform {
+        let from = match self {
+            Arrow::Straight { from, to: _ } => from,
+            Arrow::Bezier {
+                from,
+                to: _,
+                control_from: _,
+                control_to: _,
+            } => from,
+        };
+
+        Transform::from_xyz(from.x, from.y, Z_LEVEL_ARROWS)
+    }
+}
+
+struct Arrowhead {
+    left: Vec2,
+    right: Vec2,
+    point: Vec2,
+}
+const ROTATE_PLUS_45: Vec2 = Vec2::new(f32::consts::FRAC_1_SQRT_2, f32::consts::FRAC_1_SQRT_2);
+const ROTATE_MINUS_45: Vec2 = Vec2::new(f32::consts::FRAC_1_SQRT_2, -f32::consts::FRAC_1_SQRT_2);
+fn calc_arrowhead(arrow: &Arrow) -> Option<Arrowhead> {
+    let (from, to) = match arrow {
+        Arrow::Straight { from, to } => (*from, *to),
         Arrow::Bezier {
             from: _,
             to,
             control_from: _,
             control_to,
-        } => calculate_arrowhead(control_to, to),
+        } => (*control_to, *to),
     };
 
+    (from - to).try_normalize().map(|direction| {
+        let right = to + ROTATE_PLUS_45.rotate(direction) * SIZE_SCALING_FACTOR * 0.5;
+        let left = to + ROTATE_MINUS_45.rotate(direction) * SIZE_SCALING_FACTOR * 0.5;
+        Arrowhead {
+            right,
+            left,
+            point: to,
+        }
+    })
+}
+
+fn calc_arrow_path(arrow: &Arrow) -> Path {
+    let arrow = arrow.localized();
     let mut arrow_builder = PathBuilder::new();
     match arrow {
         Arrow::Straight { from, to } => {
@@ -72,56 +141,13 @@ pub fn spawn_arrow(arrow: Arrow, commands: &mut Commands) {
             arrow_builder.cubic_bezier_to(control_from, control_to, to);
         }
     }
-    if let Some(arrow_head) = arrow_head_result {
+    if let Some(arrow_head) = calc_arrowhead(&arrow) {
         arrow_builder.move_to(arrow_head.point);
         arrow_builder.line_to(arrow_head.right);
         arrow_builder.move_to(arrow_head.point);
         arrow_builder.line_to(arrow_head.left);
-    } else {
-        println!("No arrowhead!")
     }
     let arrow_path = arrow_builder.build();
-    let arrow = GeometryBuilder::new().add(&arrow_path).build();
-    commands.spawn((
-        ShapeBundle {
-            path: arrow,
-            transform: Transform::from_xyz(0., 0., Z_LEVEL_ARROWS),
-            ..default()
-        },
-        Stroke::new(BLACK, 10.),
-    ));
-    //     .observe(on_unit_grabbed)
-    //     .observe(on_unit_dragged);
-}
 
-pub enum Arrow {
-    Straight {
-        from: Vec2,
-        to: Vec2,
-    },
-    Bezier {
-        from: Vec2,
-        to: Vec2,
-        control_from: Vec2,
-        control_to: Vec2,
-    },
-}
-
-struct Arrowhead {
-    left: Vec2,
-    right: Vec2,
-    point: Vec2,
-}
-const ROTATE_PLUS_45: Vec2 = Vec2::new(f32::consts::FRAC_1_SQRT_2, f32::consts::FRAC_1_SQRT_2);
-const ROTATE_MINUS_45: Vec2 = Vec2::new(f32::consts::FRAC_1_SQRT_2, -f32::consts::FRAC_1_SQRT_2);
-fn calculate_arrowhead(from: Vec2, to: Vec2) -> Option<Arrowhead> {
-    (from - to).try_normalize().map(|direction| {
-        let right = to + ROTATE_PLUS_45.rotate(direction) * SIZE_SCALING_FACTOR * 0.5;
-        let left = to + ROTATE_MINUS_45.rotate(direction) * SIZE_SCALING_FACTOR * 0.5;
-        Arrowhead {
-            right,
-            left,
-            point: to,
-        }
-    })
+    GeometryBuilder::new().add(&arrow_path).build()
 }
